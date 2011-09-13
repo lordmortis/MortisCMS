b875035dcf65468e50c63c79a0e66d2206dcd549
Brendan Ragan <lordmortis@Brendans-MacBook-Pro.local>
2011-09-13 14:07:11 +0800

    Rails3 Routing DSL

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
-Rails.application.routes.draw do |map|
-	map.connect 'page/:id', :controller => "content_viewer", :action => "page"
-	map.connect 'blocks', :controller => "content_viewer", :action => "blocks"	
-	map.connect 'article/:id', :controller => "content_viewer", :action => "content"	
-	map.connect 'tag/:id', :controller => "content_viewer", :action => "tag"	
-  map.resources :content_blocks, :collection => { :preview => :post }, :member => { :publish => :get, :unpublish => :get}
-  map.resources :content_tags
-  map.resources :content_pages	
+Rails.application.routes.draw do 
+	match 'page/:id' => 'content_viewer#page'
+	match 'blocks' => 'content_viewer#blocks'
+	match 'article/:id' => 'content_viewer#content'
+	match 'tag/:id' => 'content_viewer#tag'
+	
+	resources :content_blocks do
+		collection do
+			post :preview
+		end
+
+		member do
+			get :publish
+			get :unpublish
+		end
+	end
+
+	resources :content_tags
+	resources :content_pages	
 end
