c19324dcaaf868ad77cc58d0d556955f11de2f7f
Brendan Ragan <lordmortis@gmail.com>
2012-02-07 16:11:20 +0800

    fixes to generator so it works properly

--- lib/generators/mortiscms/install_generator.rb
+++ lib/generators/mortiscms/install_generator.rb
@@ -2,11 +2,15 @@ require 'rails/generators'
 require 'rails/generators/migration'
 
 module Mortiscms
-	module Generators
-		class InstallGenerator < Rails::Generators::Base
-			def copy_initializer
-				copy_file 'mortiscms_initializer.rb', 'config/initializers/mortiscms.rb'
-			end
+	class InstallGenerator < Rails::Generators::Base
+		def self.source_root
+			@source_root ||= File.join(File.dirname(__FILE__), 'templates')
+		end
+
+		def copy_initializer
+			copy_file 'mortiscms_initializer.rb', 'config/initializers/mortiscms.rb'
+			copy_file 'publish_block.html.erb', 'views/content_mailer/publish_block.html.erb'
+			copy_file 'publish_block.text.erb', 'views/content_mailer/publish_block.text.erb'
 		end
 	end
 end
