0dc14fe6d083891b6199e1617486cbff00279183
Brendan Ragan <lordmortis@gmail.com>
2015-01-16 22:17:36 +0800

    restrict content mailer when running in development or staging

--- app/mailers/content_mailer.rb
+++ app/mailers/content_mailer.rb
@@ -1,11 +1,37 @@
 class ContentMailer < ActionMailer::Base
-	default from: Mortiscms.config.publish_from_friendly_address, 
-			return_path: Mortiscms.config.publish_from_address
-  	helper :mortiscms
+  default from: Mortiscms.config.publish_from_friendly_address, 
+      return_path: Mortiscms.config.publish_from_address
+    helper :mortiscms
 
-  	def publish_block(block, name, email)
-  		@block = block
-  		@name = name
-  		mail(:to => email, subject: block.title)
-  	end
+  def mail(headers = {}, &block)
+    byebug
+    if Rails.env == "staging" || Rails.env == "development"
+      if request.env["action_controller.instance"].methods.include? :current_user
+        current_user = request.env["action_controller.instance"].current_user
+      end
+      if current_user.present?
+        headers["X-original-to"] = headers[:to]
+        headers[:to] = current_user.email
+        if headers[:cc].present?
+          headers["X-original-cc"] = headers[:cc]
+          headers[:cc] = nil
+        end
+        if headers[:bcc].present?
+          headers["X-original-bcc"] = headers[:bcc]
+          headers[:bcc] = nil
+        end
+        super(headers)
+      else
+        print "WARNING: COULD NOT FIND CURRENT USER\n"
+      end
+    else
+      super(headers)
+    end
+  end
+
+  def publish_block(block, name, email)
+    @block = block
+    @name = name
+    mail(:to => email, subject: block.title)
+  end
 end
